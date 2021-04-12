c10f9bf89313d5509e6fc4f0adb683d1eaf5dd6f
umangU <72771903+umangU@users.noreply.github.com>
2021-04-12 21:36:29 +1000

    Code Updated

--- ANN.r
+++ ANN.r
@@ -23,4 +23,5 @@ plot(fleece_model)
 model_results <- compute(fleece_model, fleece_test[1:15])
 predicted_adultwool <- model_results$net.result
 #Compute the variance of predicted_adultwool
+#Compute the covariance of predicted_adultwool and fleece_test$Adultwool
 cor(predicted_adultwool, fleece_test$AdultWool) 
