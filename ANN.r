72784b26c598c85b2a1f9203a3828591a9ba35bd
umangU <72771903+umangU@users.noreply.github.com>
2021-04-22 01:29:04 +1000

    Code Updated

--- ANN.r
+++ ANN.r
@@ -1,27 +1,27 @@
-#Importing the neural net
+# Importing the neural net
 library(neuralnet)
-#Reading the fleece dataset
+# Reading the fleece dataset
 fleece <- read_xlsx(file.choose())
-#Viewing the fleece dataset variables and respective values
+# Viewing the fleece dataset variables and respective values
 str(fleece)
-#Normalizing the dataset with mean 0 and standard deviation 1
+# Normalizing the dataset with mean 0 and standard deviation 1
 normalize <- function(x){
  return((x-min(x))/(max(x)-min(x)))
 }
 fleece_norm <- as.data.frame(lapply(fleece, normalize))
 str(fleece_norm)
-#Setting the seed to get the same output
+# Setting the seed to get the same output
 set.seed(430)
-#Dividing the dataset into train and test
+# Dividing the dataset into train and test
 fleece_train <- fleece_norm[1:3000,]
 fleece_test <- fleece_norm[3001:4000,]
-#Training the neural netowrk over the train dataset
+# Training the neural netowrk over the train dataset
 fleece_model <- neuralnet(AdultWool~., data=fleece_train, hidden=1)
-#Plotting the trained neural network
+# Plotting the trained neural network
 plot(fleece_model)
-#Predicting the correlation coefficient of Adultwool
+# Predicting the correlation coefficient of Adultwool
 model_results <- compute(fleece_model, fleece_test[1:15])
 predicted_adultwool <- model_results$net.result
-#Compute the variance of predicted_adultwool
-#Compute the covariance of predicted_adultwool and fleece_test$Adultwool
+# Compute the variance of predicted_adultwool
+# Compute the covariance of predicted_adultwool and fleece_test$Adultwool
 cor(predicted_adultwool, fleece_test$AdultWool) 
