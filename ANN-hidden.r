e6e3ee926594d31205a3f6bac0d0b6e8a6899cd5
umangU <72771903+umangU@users.noreply.github.com>
2021-06-16 00:28:32 +1000

    Code Updated

--- ANN-hidden.r
+++ ANN-hidden.r
@@ -14,11 +14,11 @@ str(fleece_norm)
 #dividing the dataset into train and test
 fleece_train <- fleece_norm[1:3000,]
 fleece_test <- fleece_norm[3001:4000,]
-#declaring empty vectors to store the values of correlation coefficient and
+# declaring empty vectors to store the values of correlation coefficient and
 hidden units respectively
 co <- vector(,7)
 i_values <- vector(,7)
-#for loop to train the neural network by increasing the number of hidden
+# for loop to train the neural network by increasing the number of hidden
 units simultaneously
 for(i in 1:7)
 {
@@ -29,4 +29,4 @@ hidden=i,stepmax=1e7)
  co[i] <- as.vector(cor(predicted_adultwool, fleece_test$AdultWool))
  i_values[i]<-i
 }
-#plotting the graph of correlation coefficient vs the number of hidden 
+# plotting the graph of correlation coefficient vs the number of hidden 
